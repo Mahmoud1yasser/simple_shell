4c2e6110cb4d5e69762dec1393416da71cb45a0e
Abdelrahman-Hussien-7200 <abdhus5472@gmail.com>
2023-08-23 08:12:33 -0700

    abdo

--- main.c
+++ main.c
@@ -14,61 +14,51 @@
 
 int main(int ac, char **argv)
 {
-	char *prompt = "(ABDO_Shell) $ ", *lineptr = NULL, *lineptr_copy = NULL;
+	char *prompt = "$", *lineptr = NULL, *lineptr_copy = NULL;
 	size_t n = 0;
 	ssize_t nchars_read;
 	const char *delim = " \n";
 	int num_tokens = 0, i;
 	char *cwd = NULL, *directory, *token;
-
 	(void)ac;
-
 	while (1)
 	{
 	cwd = getcwd(NULL, 0);
 	if (cwd != NULL)
 	{
 		printf("(%s) %s", cwd, prompt);
-		free(cwd);
-	}
+		free(cwd); }
 	else
 	{
 		perror("getcwd");
-		printf("%s", prompt);
-	}
+		printf("%s", prompt); }
 	nchars_read = getline(&lineptr, &n, stdin);
 	if (nchars_read == -1)
 	{
 		printf("Exiting shell....\n");
-		break;
-	}
+		break; }
 	if (strcmp(lineptr, "exit\n") == 0)
 	{
 		printf("Exiting shell....\n");
-		break;
-	}
+		break; }
 	if (strncmp(lineptr, "cd", 2) == 0)
 	{
 		directory = lineptr + 3;
 		directory[strcspn(directory, "\n")] = '\0';
 		if (chdir(directory) != 0)
 			perror("cd");
-		continue;
-	}
+		continue; }
 	lineptr_copy = malloc(sizeof(char) * nchars_read);
 	if (lineptr_copy == NULL)
 	{
 		perror("tsh: memory allocation error");
-		return (-1);
-	}
+		return (-1); }
 	strcpy(lineptr_copy, lineptr);
 	token = strtok(lineptr, delim);
-
 	while (token != NULL)
 	{
 		num_tokens++;
-		token = strtok(NULL, delim);
-	}
+		token = strtok(NULL, delim); }
 	num_tokens++;
 	argv = malloc(sizeof(char *) * num_tokens);
 	token = strtok(lineptr_copy, delim);
@@ -76,14 +66,11 @@ int main(int ac, char **argv)
 	for (i = 0; token != NULL; i++)
 	{
 		argv[i] = malloc(sizeof(char) * strlen(token));
-
 		strcpy(argv[i], token);
-		token = strtok(NULL, delim);
-	}
+		token = strtok(NULL, delim); }
 	argv[i] = NULL;
 	execmd(argv);
-	free(lineptr_copy);
-	}
+	free(lineptr_copy); }
 	free(lineptr);
 	return (0);
 }
