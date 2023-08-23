d6bd5ffb0beae6013ef53f469f0241e322c9a6b8
Abdelrahman-Hussien-7200 <abdhus5472@gmail.com>
2023-08-23 15:35:20 -0700

    abdo

--- errors1.c
+++ errors1.c
@@ -1,40 +1,45 @@
 #include "shell.h"
 
 /**
- * _erratoi - converts a string to an integer
+ * _erratoi - Entery point
+ *
  * @s: the string to be converted
- * Return: 0 if no numbers in string, converted number otherwise
- *       -1 on error
- */
+ *
+ * Return: 0
+*/
+
 int _erratoi(char *s)
 {
-	int i = 0;
-	unsigned long int result = 0;
+	int j = 0;
+	unsigned long int res = 0;
 
 	if (*s == '+')
 		s++;  /* TODO: why does this make main return 255? */
-	for (i = 0;  s[i] != '\0'; i++)
+	for (j = 0;  s[j] != '\0'; j++)
 	{
-		if (s[i] >= '0' && s[i] <= '9')
+		if (s[j] >= '0' && s[j] <= '9')
 		{
-			result *= 10;
-			result += (s[i] - '0');
-			if (result > INT_MAX)
+			res *= 10;
+			res += (s[j] - '0');
+			if (res > INT_MAX)
 				return (-1);
 		}
 		else
 			return (-1);
 	}
-	return (result);
+	return (res);
 }
 
 /**
- * print_error - prints an error message
- * @info: the parameter & return info struct
- * @estr: string containing specified error type
- * Return: 0 if no numbers in string, converted number otherwise
- *        -1 on error
- */
+ * print_error - Entery point
+ *
+ * @info: character
+ *
+ * @estr: string containing
+ *
+ * Return: 0
+*/
+
 void print_error(info_t *info, char *estr)
 {
 	_eputs(info->fname);
@@ -47,16 +52,19 @@ void print_error(info_t *info, char *estr)
 }
 
 /**
- * print_d - function prints a decimal (integer) number (base 10)
- * @input: the input
- * @fd: the filedescriptor to write to
+ * print_d - Entery point
+ *
+ * @input: integer
+ *
+ * @fd: interger
  *
  * Return: number of characters printed
- */
+*/
+
 int print_d(int input, int fd)
 {
 	int (*__putchar)(char) = _putchar;
-	int i, count = 0;
+	int j, count = 0;
 	unsigned int _abs_, current;
 
 	if (fd == STDERR_FILENO)
@@ -70,14 +78,14 @@ int print_d(int input, int fd)
 	else
 		_abs_ = input;
 	current = _abs_;
-	for (i = 1000000000; i > 1; i /= 10)
+	for (j = 1000000000; j > 1; j /= 10)
 	{
-		if (_abs_ / i)
+		if (_abs_ / j)
 		{
-			__putchar('0' + current / i);
+			__putchar('0' + current / j);
 			count++;
 		}
-		current %= i;
+		current %= j;
 	}
 	__putchar('0' + current);
 	count++;
@@ -86,13 +94,17 @@ int print_d(int input, int fd)
 }
 
 /**
- * convert_number - converter function, a clone of itoa
- * @num: number
- * @base: base
- * @flags: argument flags
+ * convert_number - Entery point
+ *
+ * @num: integer
+ *
+ * @base: integer
+ *
+ * @flags: argument flags integer
  *
  * Return: string
- */
+*/
+
 char *convert_number(long int num, int base, int flags)
 {
 	static char *array;
@@ -122,19 +134,21 @@ char *convert_number(long int num, int base, int flags)
 }
 
 /**
- * remove_comments - function replaces first instance of '#' with '\0'
+ * remove_comments - Entery point
+ *
  * @buf: address of the string to modify
  *
  * Return: Always 0;
- */
+*/
+
 void remove_comments(char *buf)
 {
-	int i;
+	int j;
 
-	for (i = 0; buf[i] != '\0'; i++)
-		if (buf[i] == '#' && (!i || buf[i - 1] == ' '))
+	for (j = 0; buf[j] != '\0'; j++)
+		if (buf[j] == '#' && (!j || buf[j - 1] == ' '))
 		{
-			buf[i] = '\0';
+			buf[j] = '\0';
 			break;
 		}
 }
